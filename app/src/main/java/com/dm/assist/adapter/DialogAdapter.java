2879b84e0e156c878ef056c83b7434c70f3d04ac
dhakim87 <dhakimdahakim@gmail.com>
2023-03-22 14:59:09 -0700

    Colored aligned bubbles in dialogs

--- app/src/main/java/com/dm/assist/adapter/DialogAdapter.java
+++ app/src/main/java/com/dm/assist/adapter/DialogAdapter.java
@@ -1,8 +1,10 @@
 package com.dm.assist.adapter;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,11 +28,33 @@ public class DialogAdapter extends RecyclerView.Adapter<DialogAdapter.ViewHolder
         this.onItemClickListener = onItemClickListener;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.dialog_item, parent, false);
+        TextView messageTextView = view.findViewById(R.id.messageTextView);
+
+        System.out.println("viewType" + viewType);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT
+                , LinearLayout.LayoutParams.WRAP_CONTENT);
+        if (viewType == 0)
+        {
+            params.gravity = Gravity.START;
+            messageTextView.setBackgroundResource(R.drawable.message_background_user);
+        }
+        else
+        {
+            params.gravity = Gravity.END;
+            messageTextView.setBackgroundResource(R.drawable.message_background_gpt);
+        }
+        messageTextView.setLayoutParams(params);
+
         return new ViewHolder(view, onItemClickListener);
     }
 
